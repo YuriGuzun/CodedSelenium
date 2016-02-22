53c843ef6a6b2638f4d434cc8d2ac759370ee2de
YuriGuzun <TA5ar1p5>
2016-02-23 01:12:25 +0300

    BasicCheck_HtmlButton_ByInnerText

--- CodedSelenium.Test/BasicCheck.cs
+++ CodedSelenium.Test/BasicCheck.cs
@@ -8,29 +8,33 @@ namespace CodedSelenium.Test
     [TestClass]
     public class BasicCheck
     {
+        private static string PathToPage = Directory.GetCurrentDirectory() + "\\TestPages\\TestPage.html";
+        private static BrowserWindow BrowserWindow = BrowserWindow.Launch(PathToPage);
+
         [TestMethod]
         public void BasicCheck_HtmlEdit()
         {
-            string pathToPage = Directory.GetCurrentDirectory() + "\\TestPages\\TestPage.html";
-            BrowserWindow bw = BrowserWindow.Launch(pathToPage);
-
-            HtmlDiv div = new HtmlDiv(bw);
+            HtmlDiv div = new HtmlDiv(BrowserWindow);
             div.SearchProperties.Add(HtmlControl.PropertyNames.Id, "loginFields");
             HtmlEdit edit = new HtmlEdit(div);
 
             string value = "banana";
             edit.Text = value;
             edit.Text.ShouldBeEqualTo(value);
+        }
 
-            HtmlButton button = new HtmlButton(div);
+        [TestMethod]
+        public void BasicCheck_HtmlButton_ByInnerText()
+        {
+            HtmlButton button = new HtmlButton(BrowserWindow);
             button.SearchProperties.Add(HtmlButton.PropertyNames.InnerText, "Second Button");
             button.Click();
 
-            HtmlControl id = new HtmlControl(bw);
+            HtmlControl id = new HtmlControl(BrowserWindow);
             id.SearchProperties.Add(HtmlControl.PropertyNames.TagName, "p");
             id.SearchProperties.Add(HtmlControl.PropertyNames.Id, "id");
 
-            HtmlControl action = new HtmlControl(bw);
+            HtmlControl action = new HtmlControl(BrowserWindow);
             action.SearchProperties.Add(HtmlControl.PropertyNames.TagName, "p");
             action.SearchProperties.Add(HtmlControl.PropertyNames.Id, "action");
 
