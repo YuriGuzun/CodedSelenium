db59ff054c4004768deb25a486c51c48599d4958
codri <igor.cohanovschi@gmail.com>
2016-05-29 19:29:16 +0300

    Fixed the StyleCop issue.

--- CodedSelenium.Test/BasicTest.cs
+++ CodedSelenium.Test/BasicTest.cs
@@ -6,7 +6,8 @@ using OpenQA.Selenium.Support.UI;
 using System;
 using System.IO;
 
-namespace CodedSelenium.Test {
+namespace CodedSelenium.Test
+{
     public class BasicTest
     {
         public const string PageName = "BasicTestPage.html";
