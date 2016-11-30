bb6a367573085e5f4906a1e13867040ac13bf8f7
Codri <igor.cohanovschi@gmail.com>
2016-11-30 21:20:55 +0200

    Adding updated Playback
    Adding updated Playback class.

--- CodedSelenium/Playback.cs
+++ CodedSelenium/Playback.cs
@@ -5,6 +5,21 @@ namespace CodedSelenium
 {
     public static class Playback
     {
+        /// <summary>
+        /// Property added for Compatibility Purposes Only
+        /// Doesn't work
+        /// </summary>
+        public static event EventHandler<PlaybackErrorEventArgs> PlaybackError
+        {
+            add
+            {
+            }
+
+            remove
+            {
+            }
+        }
+
         public static int Wait(int thinkTimeMilliseconds)
         {
             Thread.Sleep(thinkTimeMilliseconds);
