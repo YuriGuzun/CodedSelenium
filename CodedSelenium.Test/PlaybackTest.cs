a8195576499badcd2b8809e1383bfec02811006d
Codri <igor.cohanovschi@gmail.com>
2016-11-30 21:27:44 +0200

    Added a simple test for asserting that PlaybackErrorHandler doesn't throw
    Added a simple test for asserting that PlaybackErrorHandler doesn't
    throw. Not entirely sure it's needed. Though, it could be removed later
    if not needed.

---
+++ CodedSelenium.Test/PlaybackTest.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+
+namespace CodedSelenium.Test
+{
+    [TestFixture]
+    public class PlaybackTest : BasicTest
+    {
+        [Test]
+        public void PlaybackTest_RegisterErrorCallback_DoesntThrow()
+        {
+            assignPlaybackError.ShouldNotThrow();
+        }
+
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
